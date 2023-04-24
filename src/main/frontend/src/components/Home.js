1fa405efbd3bf59adc7454d8e0fd35a4b046fee7
sujin-create <qsoojinq@naver.com>
2023-04-24 18:50:43 +0900

    좌표를 통한 영역 구분 및 페이징 Feat Home.js map과area 그리고 link를 통한 좌표에 따른 페이징기능적용 틀 잡기

--- src/main/frontend/src/components/Home.js
+++ src/main/frontend/src/components/Home.js
@@ -1,19 +1,28 @@
 import React from "react";
 import "../components/Home.css";
-import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
-    let navigate = useNavigate();
-
-    const Seoul = (e) => {
-        e.preventDefault();
-        navigate('/Seoul');
+    const MapAreaLinks = ()=> {
+        return (
+            <map name="map">
+                <Link to="/Seoul">
+                    <area shape="rect" coords="10,10,300,300" />
+                </Link>
+                <Link to="/Busan">
+                    <area shape="rect" coords="300,300,800,800" />
+                </Link>
+            </map>
+        );
     }
 
     return(
         <div className="ImageDiv">
-            <a onClick={Seoul}><img alt="map" src="img/map1.png" /></a>
+            <Link to="/">
+                <img src="img/map1.png" useMap="#map" alt="Map image" />
+            </Link>
+            <MapAreaLinks />
         </div>
     )
 }
