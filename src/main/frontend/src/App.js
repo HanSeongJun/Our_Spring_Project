2ccb8f415b64507824c25cde25736d38dbc793bf
sujin-create <qsoojinq@naver.com>
2023-04-27 21:07:14 +0900

    구 별로 스팟 리스트 보여주기 Feat Gu.js city를 누르면 gu에 들어가고 gu를 누르면 해당 페이지에서 오른쪽에 포함된 spot을 보여주게 구조변경

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./components/Map/Home";
+import City from "./components/Map/city/City";
+import Gu from "./components/Map/gu/Gu";
 import Nav from "./components/Nav/nav";
-import City from "./components/city/City";
 
 function App() {
     return(
         <BrowserRouter>
             <Nav />
             <Routes>
-                <Route path='/' element = { <Home />}></Route>
+                <Route path='/home' element = { <Home />}></Route>
                 <Route path="/city/:code" element = { <City />} />
+                <Route path="/city/:city_code/:gu_code" element = { <Gu />} />
             </Routes>
         </BrowserRouter>
     )
