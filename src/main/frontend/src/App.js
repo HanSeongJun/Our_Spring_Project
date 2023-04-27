c568e90ffe899ca098eac1f455206d96503dccc7
sujin-create <qsoojinq@naver.com>
2023-04-27 21:52:17 +0900

    필요없는 요소 삭제 Remove Nav.js/img

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,12 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Map/Home";
 import City from "./components/Map/city/City";
 import Gu from "./components/Map/gu/Gu";
-import Nav from "./components/Nav/nav";
 
 function App() {
     return(
         <BrowserRouter>
-            <Nav />
             <Routes>
                 <Route path='/home' element = { <Home />}></Route>
                 <Route path="/city/:code" element = { <City />} />
