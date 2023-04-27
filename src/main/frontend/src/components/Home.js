3d72a5d24433d37f48a2e6ffa08b997c94d67861
sujin-create <qsoojinq@naver.com>
2023-04-27 11:45:59 +0900

    시도별 지도 영역구분을 위해 SVG file 사용 Refactor Map.js map area tag로 나누고 영역별로 action처리를 하기에는 한계가 있어서 SVG 파일로 영역구분후 코드별로 action처리로 변경

--- src/main/frontend/src/components/Home.js
+++ src/main/frontend/src/components/Home.js
@@ -1,28 +1,12 @@
 import React from "react";
 import "../components/Home.css";
-import { Link } from 'react-router-dom';
+import Map from "./Map/Map";
 
 const Home = () => {
 
-    const MapAreaLinks = ()=> {
-        return (
-            <map name="map">
-                <Link to="/Seoul">
-                    <area shape="rect" coords="10,10,300,300" />
-                </Link>
-                <Link to="/Busan">
-                    <area shape="rect" coords="300,300,800,800" />
-                </Link>
-            </map>
-        );
-    }
-
     return(
-        <div className="ImageDiv">
-            <Link to="/">
-                <img src="img/map1.png" useMap="#map" alt="Map image" />
-            </Link>
-            <MapAreaLinks />
+        <div>
+            <Map />
         </div>
     )
 }
