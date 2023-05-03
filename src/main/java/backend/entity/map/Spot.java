4d4e44f9feb2c12b1bc20da557148c5ed7125149
sujin-create <qsoojinq@naver.com>
2023-05-03 13:49:32 +0900

    entity추가 및 repository추가 Feat : repository, spot.java board는 이후 추가할 예정

--- src/main/java/backend/entity/map/Spot.java
+++ src/main/java/backend/entity/map/Spot.java
@@ -1,15 +1,37 @@
 package backend.entity.map;
 
+import backend.entity.board.Board;
+import com.sun.istack.NotNull;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Spot {
 
     @Id @GeneratedValue
     private Long Id;
+    @NotNull
+    private String spotName;
+    private String comment;
+
+    //spot:gu -> 다:1
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="gu_id")
+    private Gu gu;
+
+    //spot:board -> 1:다
+    @OneToMany(mappedBy = "board")
+    private List<Board> baordList = new ArrayList<>();
 
     public Spot(){
 
     }
-    //연관관계 편의메서드 필요
+
+    //연관관계 편의메서드
+    public void changeGu(Gu gu){
+        this.gu = gu;
+        gu.getSpotlist().add(this);
+    }
 }
