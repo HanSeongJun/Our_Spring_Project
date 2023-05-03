ae78e843c4a9a8a4ed5ebceb97af7879f0da9c41
sujin-create <qsoojinq@naver.com>
2023-05-04 04:19:39 +0900

    json 형식 맞춰서 return Refactor CityController data로 DTO List를 감싸서 반환

--- src/main/java/backend/controller/map/CityController.java
+++ src/main/java/backend/controller/map/CityController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -17,8 +18,12 @@ public class CityController {
     private final CityServiceImpl cityService;
 
     @GetMapping("/citymap")
-    public List<CityMapInfoDto> cityMap(){
-        return cityService.getCityMap();
+    public HashMap<String, List> cityMap(){
+        List<CityMapInfoDto> cityMap = cityService.getCityMap();
+        HashMap<String , List> cityMapResult = new HashMap<>();
+        cityMapResult.put("data", cityMap);
+
+        return cityMapResult;
     }
 
 }
