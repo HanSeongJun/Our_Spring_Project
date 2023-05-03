06c81054d538427a4250cb1f5ef1345b46a05064
sujin-create <qsoojinq@naver.com>
2023-05-03 13:49:32 +0900

    entity추가 및 repository추가 Feat : repository, spot.java board는 이후 추가할 예정

---
+++ src/main/java/backend/repository/map/CityRepository.java
@@ -0,0 +1,7 @@
+package backend.repository.map;
+
+import backend.entity.map.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+}
---
+++ src/main/java/backend/repository/map/GuRepository.java
@@ -0,0 +1,7 @@
+package backend.repository.map;
+
+import backend.entity.map.Gu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuRepository extends JpaRepository<Gu, Long> {
+}
