b33dc3a7ebefe9b9483ac2d5db3529235a164fd9
sujin-create <qsoojinq@naver.com>
2023-05-06 04:50:00 +0900

    frontend에서 backend api 연결 Feat Gu.js Gu를 클릭하면 관련 spot을 보여주는 spotlist 출력용 api 연결 성공

--- src/main/frontend/src/components/Map/gu/Gu.js
+++ src/main/frontend/src/components/Map/gu/Gu.js
@@ -1,21 +1,32 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import Seoul from "../city/Seoul";
 import "./styles/Gu.css";
 
+const API_URL = 'http://localhost:8080/map/spotlist';
+//현재는 서울만 들어있음 -> test용 -> parameter를 city_code,gu_code로 변경예정
+
 const Gu = () => {
 
     const { city_code, gu_code } = useParams();
+    const [data, setData] = useState([]);
 
     //넘어온 코드에 따라서 다른 결과를 보여주는 로직을 작성해야함
     console.log("gu.js에 넘어온 city/gu code parameter-------->  ",city_code, gu_code);
 
-    //gu_code를 통해서 backend에 spot data를 get 해서 보여주는 코드를 작성해야함
-    const [spot, setSpot] = useState({
-        spotId: 0,
-        name: "spot name"
-    });
-
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`${API_URL}?guCode=${gu_code}`);//url에 city_code, gu_code를 넘기는 것으로 변경예정
+                const json = await response.json();
+                setData(json.data);
+                console.log('Fetched data:', json.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData();
+    }, []);
 
     return (
         <div className="Gu">
@@ -24,23 +35,16 @@ const Gu = () => {
                 {city_code === "CD11" && gu_code === "CD11170" && <Seoul />}
             </div>
             <div className="Spot">
-                <h1>Spot list</h1>
-                <div>
-                    Title:{spot.name}<br/>
-                    Imagelist : image를 넣습니다.
-                </div>
-                <div>
-                    Title:{spot.name}<br/>
-                    Imagelist : image를 넣습니다.
-                </div>
-                <div>
-                    Title:{spot.name}<br/>
-                    Imagelist : image를 넣습니다.
-                </div>
+                {data.map((item, index) => (
+                    <div key={index}>
+                        <p>Spot Name: {item.spotName}</p>
+                        <p>Comment: {item.comment}</p>
+                    </div>
+                ))}
             </div>
         </div>
-    )
+    );
 
-}
+};
 
 export default Gu;
