bb2808532bca9ebe604cd02d24ae1e8fa47adeba
sujin-create <qsoojinq@naver.com>
2023-05-06 04:46:41 +0900

    spot list 출력을 위한 파라미터 변경 Fix GuService GuServiceImpl GuRepository GuCode로 변경

--- src/main/java/backend/repository/map/GuRepository.java
+++ src/main/java/backend/repository/map/GuRepository.java
@@ -3,5 +3,8 @@ package backend.repository.map;
 import backend.entity.map.Gu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GuRepository extends JpaRepository<Gu, Long> {
+    Optional<Gu> findByGuCode(String guCode);
 }
