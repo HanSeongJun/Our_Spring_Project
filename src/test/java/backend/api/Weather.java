856059f15952fc8a2b27b1457f7bd459f2549c40
sujin-create <qsoojinq@naver.com>
2023-05-05 20:29:17 +0900

    공공 api call test Test Weather 날씨정보 공공 api call test / env정보는 api.yml에 설정하여 보안유지

---
+++ src/test/java/backend/api/Weather.java
@@ -0,0 +1,77 @@
+package backend.api;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@SpringBootTest(properties = "spring.config.location=" +
+        "classpath:/application.yaml" +
+        ",classpath:/api.yml"
+)
+public class Weather {
+
+    private final String serviceKey;
+    private final String endPoint;
+
+    public Weather(@Value("${api.serviceKey}") String serviceKey,
+                   @Value("${api.endPoint}") String endPoint){
+        this.serviceKey = serviceKey;
+        this.endPoint = endPoint;
+    }
+
+    @Test
+    public void WeatherApiCallTest() throws IOException {
+
+        String buildUrl = buildUrl();
+
+        URL url = new URL(buildUrl);
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+
+        System.out.println("Response code: " + conn.getResponseCode());
+        System.out.println("Response message: " + conn.getResponseMessage());
+
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+
+        System.out.println(sb.toString());
+
+
+    }
+
+    private String buildUrl() throws UnsupportedEncodingException {
+
+        StringBuilder urlBuilder = new StringBuilder(endPoint); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + serviceKey); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON) Default: XML*/
+        urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode("20230505", "UTF-8")); /*‘21년 6월 28일 발표*/
+        urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode("0600", "UTF-8")); /*06시 발표(정시단위) */
+        urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode("55", "UTF-8")); /*예보지점의 X 좌표값*/
+        urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode("127", "UTF-8")); /*예보지점의 Y 좌표값*/
+        return urlBuilder.toString();
+    }
+}
