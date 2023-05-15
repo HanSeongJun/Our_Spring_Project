327a5b7c16671fc85e3f4b0c00ce6357ab30ac2c
Zzoons <67749318+HanSeongJun@users.noreply.github.com>
2023-05-15 17:47:56 +0900

    새 비밀번호 입력 페이지 구성 요소 추가 FEAT NewUserPassword.js, NewUserPassword.css

--- src/main/frontend/src/components/User/NewUserPassword.js
+++ src/main/frontend/src/components/User/NewUserPassword.js
@@ -1,13 +1,28 @@
 import * as React from 'react';
 
 import './styles/NewUserPassword.css';
-import {Typography} from "@mui/material";
+import {Box, TextField, Typography} from "@mui/material";
+import Button from "@mui/material/Button";
+import {Link} from "react-router-dom";
 
 export default function NewUserPassword() {
 
     return (
-        <div>
-            <Typography >새 비밀번호 설정</Typography>
+        <div className="container">
+            <div className="containerWrapper">
+                <Typography >새 비밀번호 설정</Typography>
+                <Box className="field" sx={{ mb: 3 }}>
+                    <TextField id="password" label="비밀번호" type="password" className="input" required/>
+                </Box>
+                <Box className="field" sx={{ mb: 3 }}>
+                    <TextField id="confirmPassword" label="비밀번호 확인" type="password" className="input" required/>
+                </Box>
+                <Link to="/user/editUserComplete">
+                    <Button type="submit" variant="contained" size="large" className="input" sx={{ mb: 3 }}>
+                        비밀번호 변경하기
+                    </Button>
+                </Link>
+            </div>
         </div>
     );
 }
